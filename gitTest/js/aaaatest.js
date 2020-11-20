e4b6ed6affbb8066e736ea8b5bae612bf0e01c5d
unknown <08654@linshimuye.local>
2020-11-20 18:00:51 +0800

    first master

---
+++ gitTest/js/aaaatest.js
@@ -0,0 +1,12 @@
+// 给定一个整数数组 nums 和一个目标值 target，
+// 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+let sum = function (nums, target) {
+    let i = sums.length
+    while (i--) {//5
+        for (let x  = i-1; x > 0; x--) {//4 3 2 1
+            console.log()
+        }
+    }
+    
+}()
